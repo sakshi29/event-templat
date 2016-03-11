adac51f49fc2085662297e4fd8b8cfa2e8c9eb30
MAYANK TRIPATHI <MAYANK TRIPATHI>
2016-03-11 17:27:42 +0530

    first commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+        
